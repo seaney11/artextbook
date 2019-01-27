f77f823cc2d2c7e24636b7f8cbac86faf2fb582a
Sean McGuire <sean.mcguire415@gmail.com>
2019-01-27 11:40:45 +0000

    planets

--- app/app/src/main/java/com/artextbook/artextbook/image_nodes/solar/PlanetNode.java
+++ app/app/src/main/java/com/artextbook/artextbook/image_nodes/solar/PlanetNode.java
@@ -8,8 +8,14 @@ import java.util.concurrent.TimeUnit;
 
 public class PlanetNode extends Node {
 
-    public PlanetNode(Vector3 vector) {
+    double speed;
+
+    public PlanetNode(double radius, double angle, double speed, float size) {
         super();
+        radius = 0.4 * radius;
+        Vector3 vector = new Vector3((float)(radius * Math.cos(angle)), 0, (float)(radius * Math.sin(angle)));
+        this.setLocalScale(new Vector3(size, size, size));
+        this.speed = speed;
         this.setLocalPosition(vector);
     }
 
@@ -18,8 +24,8 @@ public class PlanetNode extends Node {
         super.onUpdate(frameTime);
         Vector3 my_vector = this.getLocalPosition();
         Vector3 keep_safe = new Vector3(my_vector);
-        my_vector.x = (float) ( Math.cos(0.1) * keep_safe.x + Math.sin(0.1) * keep_safe.z);
-        my_vector.z = (float) ( - Math.sin(0.1) * keep_safe.x + Math.cos(0.1) * keep_safe.z);
+        my_vector.x = (float) ( Math.cos(0.01 * speed) * keep_safe.x + Math.sin(0.01 * speed) * keep_safe.z);
+        my_vector.z = (float) ( - Math.sin(0.01 * speed) * keep_safe.x + Math.cos(0.01 * speed) * keep_safe.z);
         this.setLocalPosition(my_vector);
     }
 
